82d27f8e7b13de4b14c578b113e8333e5f742d56
garbahad23 <okanbagci23@hotmail.com>
2015-03-02 14:58:53 +0200

    ilk commit

---
+++ SampleMVCProject/Areas/OkanArea/OkanAreaAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace SampleMVCProject.Areas.OkanArea
+{
+    public class OkanAreaAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "OkanArea";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "OkanArea_default",
+                "OkanArea/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
