84bca37fb1e60f67c8a42b82139a1df8c20b73bb
garbahad23 <okanbagci23@hotmail.com>
2015-03-02 15:16:20 +0200

    versiyon 2 ilk commit.

---
+++ SampleMVCProject/Areas/OkanArea/Controllers/UrunController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SampleMVCProject.Areas.OkanArea.Controllers
+{
+    public class UrunController : Controller
+    {
+        //
+        // GET: /OkanArea/Urun/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
