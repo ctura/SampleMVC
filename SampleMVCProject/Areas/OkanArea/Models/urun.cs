8720e69e3489e51300b42cf26c087550d7bb37e6
garbahad23 <okanbagci23@hotmail.com>
2015-03-02 15:23:11 +0200

    ürün class eklendi.

---
+++ SampleMVCProject/Areas/OkanArea/Models/urun.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SampleMVCProject.Areas.OkanArea.Models
+{
+    public class urun
+    {
+        public int Id { get; set; }
+        public string Ad { get; set; }
+    }
+}
